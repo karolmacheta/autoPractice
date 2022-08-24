a37ad7f371e51a0953afd37ac31303735f012169
karolmacheta <karol.macheta@gmail.com>
2022-08-24 15:27:14 +0200

    added tc4, tc5

---
+++ cypress/e2e/04_LoginLogout.cy.js
@@ -0,0 +1,34 @@
+import { mainPage } from "../locators/main";
+import { signupPage } from "../locators/signup";
+import testUser from "../fixtures/User.json"
+
+describe('Test Case 4: Logout User', () => {
+
+  it('open the page and check url', () => {
+    cy.openMainPage();
+  })
+
+  it('go to login page', () => {
+    cy.get(mainPage.loginBtn).should('be.visible').click();
+    cy.url().should('eq', 'https://www.automationexercise.com/login');
+    cy.contains('Login to your account').should('be.visible')
+  })
+
+  it('login using test user credentials', () => {
+    cy.getByQaData(signupPage.loginEmailInput).should('be.visible').type(testUser.email);
+    cy.getByQaData(signupPage.loginPassInput).should('be.visible').type(testUser.pass);
+    cy.getByQaData(signupPage.loginBtn).should('be.visible').click();
+  })
+
+  it('check is user successfully logged in', () => {
+    cy.contains(`Logged in as ${testUser.name}`).should('be.visible')
+  })
+
+  it('log out', () => {
+    cy.get(mainPage.logoutBtn).should('be.visible').click();
+    cy.url().should('eq', 'https://www.automationexercise.com/logout');
+    cy.visit('/');
+    cy.contains(`Logged in as ${testUser.name}`).should('not.exist');
+  })
+})
+
